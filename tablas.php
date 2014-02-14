a5287f3c3d695d644373cec73daee569abbb0868
mstramon <master.rayu.1@gmail.com>
2014-02-13 21:30:43 -0600

    agregamos tablas
    tablas del 1 - 12

---
+++ tablas.php
@@ -0,0 +1,13 @@
+<?php
+$num1=0; $num2=0;
+$cont=1;
+for(;$num1<=12; $cont++){  
+$num1++;  echo "Tabla del $num1<br/>"; $num2=1;
+
+for($cont=1 ; $num2<=12; $cont++, $num2++){
+  
+echo " $num1 x $num2= ", $num1*$num2, '<br/r>' ; 
+}
+echo '<br/r>' ; 
+}
+?> 
